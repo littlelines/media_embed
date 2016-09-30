691de558495dbd558daf08532544cfff0dc4052d
WendyBeth <wendybeth010@gmail.com>
2016-09-30 16:46:19 -0400

    Fixes arity of podcast method to accept options

--- lib/media_embed/podcast.rb
+++ lib/media_embed/podcast.rb
@@ -1,6 +1,6 @@
 module MediaEmbed
   class Podcast
-    def self.soundcloud_template(code)
+    def self.soundcloud_template(code, options = {})
       %Q(<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/#{code}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>)
     end
   end
